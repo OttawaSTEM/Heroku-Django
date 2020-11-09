1a2e76bcd85e6a08be9115b56f81eb8b5c22fc40
Patrick Li <ottawastem@gmail.com>
2020-11-09 15:25:53 -0500

    update heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'tdgp8r)!0w*hdghi&azr$xf-5&a8b2@ust=$w4!3&x1hx1pwm5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heroku-django-skeleton.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

b3ece74b9a6259d05d1e6794a43ef986b3ae4d60
OttawaSTEM <ottawastem@gmail.com>
2022-02-03 14:48:34 -0500

    update

--- mysite/settings/deployment.py
+++ mysite/settings/deployment.py
@@ -1,4 +1,4 @@
 from .base import *
 
-DEBUG = False
+DEBUG = False           # Must run "python manage.py collect static", otherwise cause Server Error (500)
 CSRF_TRUSTED_ORIGINS = ['ottawastem-django.herokuapp.com']      # When DEBUG=False in Deployment

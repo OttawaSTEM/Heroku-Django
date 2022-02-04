4966c777ac8ed7c03c261029311909a6b03eb193
OttawaSTEM <ottawastem@gmail.com>
2022-02-03 20:05:53 -0500

    update wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,6 +6,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
+if os.environ.get('DJANGO_ENVIRONMENT') == 'Development':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
+elif os.environ.get('DJANGO_ENVIRONMENT') == 'Container':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.container')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.virtualmachine')
 
 application = get_wsgi_application()
